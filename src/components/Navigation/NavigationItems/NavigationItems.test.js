0dc42d9de777112cc3b0e98715eebd744e83c697
Raza Mehdi <srmk@outlook.com>
2018-06-06 19:57:18 +0500

    Add some tests

---
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import NavigationItems from './NavigationItems';
+import NavigationItem from './NavigationItem/NavigationItem';
+
+configure({
+    adapter: new Adapter()
+});
+
+describe('NavigationItems', () => {
+    let wrapper;
+
+    beforeEach(() => {
+        wrapper = shallow(<NavigationItems />);
+    });
+
+    it('will render two <NavigationItem /> elements if not authenticated', () => {
+        expect(wrapper.find(NavigationItem)).toHaveLength(2);
+    });
+
+    it('will render three <NavigationItem /> elements if authenticated', () => {
+        wrapper.setProps({isAuthenticated: true});
+        expect(wrapper.find(NavigationItem)).toHaveLength(3);
+    });
+});
